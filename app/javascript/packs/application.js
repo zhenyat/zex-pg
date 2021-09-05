210c67d52a0aa26016e2fc5e4af1fe97b365858b
Zhenya Telyukov <telyukov@gmail.com>
2021-09-05 22:42:52 +0300

    Initial

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,19 @@
+/***
+ *  v. 3.13:  Bootstrap 5 - jQuery + @popperjs/core
+ ***/
+ import Rails from "@rails/ujs"
+ import Turbolinks from "turbolinks"
+ import * as ActiveStorage from "@rails/activestorage"
+ import "channels"
+ 
+ Rails.start()
+ Turbolinks.start()
+ ActiveStorage.start()
+ 
+ import 'bootstrap'
+ import "@fortawesome/fontawesome-free/js/all";
+ // import "@fortawesome/fontawesome-free/css/all"
+ import '../stylesheets/application'
+ 
+require("trix")
+require("@rails/actiontext")
---
+++ config/webpack/environment.js
@@ -0,0 +1,5 @@
+/***
+ *  v. 3.13:  Bootstrap 5 - jQuery + @popperjs/core
+ ***/
+const { environment } = require('@rails/webpacker')
+module.exports = environment
